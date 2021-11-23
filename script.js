de3d6f285f7bee11fcc4dc85c5ee644428168dd7
kirkwood616 <kirkwood616@gmail.com>
2021-11-22 23:47:56 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function logRectInfo(topLeft, bottomRight) {
+  let area = (topLeft[1] - bottomRight[1]) * (bottomRight[0] - topLeft[0]);
+  let perimeter =
+    (topLeft[1] - bottomRight[1] + (bottomRight[0] - topLeft[0])) * 2;
+  if (topLeft || bottomRight < 0) {
+    Math.abs(topLeft[0]);
+    Math.abs(bottomRight[1]);
+  }
+  console.log(`Area = ${area} & Perimeter = ${perimeter}`);
+}
+
+logRectInfo([0, 5], [3, 2]);
