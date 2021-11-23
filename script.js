0abad2d0d2e687c16517380f808fe3ff98c5b1e2
kirkwood616 <kirkwood616@gmail.com>
2021-11-23 08:35:15 -0500

    Added missing code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function logRectInfo(topLeft, bottomRight) {
   let area = (topLeft[1] - bottomRight[1]) * (bottomRight[0] - topLeft[0]);
   let perimeter =
     (topLeft[1] - bottomRight[1] + (bottomRight[0] - topLeft[0])) * 2;
-  if (topLeft || bottomRight < 0) {
+  if (topLeft < 0 || bottomRight < 0) {
     Math.abs(topLeft[0]);
     Math.abs(bottomRight[1]);
   }
